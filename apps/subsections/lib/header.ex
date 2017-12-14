db5d78dd10fdf97b9e63dbd48fb4218b6aac5b9c
David Heyman <dheyman@fastmail.com>
2017-12-13 20:19:22 -0500

    Fix misc compile-time errors

--- apps/subsections/lib/header.ex
+++ apps/subsections/lib/header.ex
@@ -8,12 +8,6 @@ defmodule Header do
     :realm_id, :unk02, :unk03, :game_name, :password,
     :master_password, :turn_key
   ]
-  @type t :: %Header{
-    user_id: integer(), game_version: integer(), turn_number: integer(),
-    unk00: integer(), unk01: integer(), realm_id: integer(),
-    unk02: integer(), unk03: integer(), game_name: binary(),
-    password: binary(), master_password: binary(), turn_key: integer()
-  }
   
   def unpack(io_bytestream) do
     with <<0x01, 0x02, 0x04, 0x44, 0x4f, 0x4d,
@@ -30,10 +24,12 @@ defmodule Header do
          # passwords are additionally ciphered, but I don't need them here anyway.
          password <- DomString.read(io_bytestream),
          master_password <- DomString.read(io_bytestream),
+         <<turn_key::integer-little-32>> <- Enum.take(io_bytestream, 4),
       do: {:ok, %Header{
-        user_id:user_id, game_version:game_version, turn_number:turn_number,
-        unk00:unk00, unk01:unk01, realm_id:realm_id,
-        unk02:unk02, unk03:unk03, game_name:game_name,
-        password:password, master_password:master_password, turn_key:turn_key
+        user_id: user_id, game_version: game_version, turn_number: turn_number,
+        unk00: unk00, unk01: unk01, realm_id: realm_id,
+        unk02: unk02, unk03: unk03, game_name: game_name,
+        password: password, master_password: master_password, turn_key: turn_key
       }}
   end
+end
--- apps/subsections/lib/unpack.ex
+++ apps/subsections/lib/unpack.ex
@@ -2,50 +2,41 @@ defmodule Unpack do
   @type kind :: {:integer, :little | :big, pos_integer()}
   | {:binary} | {:binary, pos_integer()}
   | {:any}
-  @type reader :: (Enumerable.t) -> any() # might be able to make this one more specific
+  @type reader :: (Enumerable.t -> any()) # might be able to make this one more specific
   @type field :: {atom(), kind, reader}
 
   defmacro __using__(fields) do
     quote do
       defstruct unquote(for {name, _type, _reader} <- fields, do: name)
-      @type t :: %unquote(__ENV__.module){
-        unquote_splicing(
-          for {name, kind, _reader} <- fields, do: {name, spec_type(kind)}
-        )
-      }
-      unquote(makefun(fieldss, __ENV__.module))
+      unquote(make_unpack(fields, __ENV__.module))
     end
   end
 
-  defp spec_type({:integer, _, _}) do: integer()
-  defp spec_type({:binary}) do: binary()
-  defp spec_type({:binary, _}) do: binary()
-  defp spec_type({:any}) do: any()
+  defp bit_type({:integer, :little, size}), do: quote(do: integer-little-unquote(size))
+  defp bit_type({:integer, :big, size}), do: quote(do: integer-big-unquote(size))
+  defp bit_type({:binary}), do: quote(do: binary)
+  defp bit_type({:binary, size}), do: quote(do: binary-unquote(size))
 
-  defp bit_type({:integer, :little, size}) do: quote(integer-little-unquote(size))
-  defp bit_type({:integer, :big, size}) do: quote(integer-big-unquote(size))
-  defp bit_type({:binary}) do: quote(binary)
-  defp bit_type({:binary, size}) do: quote(binary-unquote(size))
-
-  defp makefun(args, module) do: funs(fields, module, [], [])
-  defp makefun([{name, kind, reader} | tail], module, acc_bind, acc_pairs) do
+  defp make_unpack(fields, module), do: make_unpack(fields, module, [], [])
+  defp make_unpack([{name, kind, reader} | tail], module, acc_bind, acc_pairs) do
     var = Macro.var(name, module)
     input = Macro.var(:io_bytestream, module)
-    
+
     bind = case kind do
       {:any} ->
-        quote(unquote(var) = unquote(reader).(unquote(input)))
+        quote(do: unquote(var) = unquote(reader).(unquote(input)))
       _ ->
-        quote(<<unquote(var)::unquote(bit_type(kind))>> = unquote(reader).(unquote(input)))
+        quote(do: <<unquote(var)::unquote(bit_type(kind))>> = unquote(reader).(unquote(input)))
+    end
 
-    pair = quote({unquote(name), unquote(var)})
-    
-    funs(tail, module,
+    pair = quote(do: {unquote(name), unquote(var)})
+
+    make_unpack(tail, module,
          [bind | acc_bind],
          [pair | acc_pairs]
     )
   end
-  defp makefun([], module, acc_bind, acc_pairs) do
+  defp make_unpack([], module, acc_bind, acc_pairs) do
     binds = Enum.reverse(acc_bind)
     pairs = Enum.reverse(acc_pairs)
     quote do
