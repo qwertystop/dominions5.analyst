2ba53fe4c1ce426131e4cb377af0912c183690c9
David Heyman <dheyman@fastmail.com>
2017-12-19 13:56:28 -0500

    small correction to unpacking/parsing in DomString

--- apps/parser/lib/unpack/dom_string.ex
+++ apps/parser/lib/unpack/dom_string.ex
@@ -5,15 +5,16 @@ defmodule Parser.Unpack.DomString do
   import Bitwise, only: [bxor: 2]
 
   def read(io_bytestream) do
-    Enum.reduce_while(
+    <<_::binary>> = Enum.reduce_while(
       io_bytestream, <<>>,
-      fn <<ch, _::binary>>, acc ->
+      fn <<ch::8>>, acc ->
         val = <<acc::binary, bxor(ch, 0x4f)>>
         if ch == 0x4f, do: {:halt, val}, else: {:cont, val}
       end)
   end
 
   def read(io_bytestream, count) do
-    for <<c::8>> <- Enum.take(io_bytestream, count), into: <<>>, do: bxor(c, 0x4f)
+    <<_::binary>> = for <<c::8>> <-
+      Enum.take(io_bytestream, count), into: <<>>, do: bxor(c, 0x4f)
   end
 end
