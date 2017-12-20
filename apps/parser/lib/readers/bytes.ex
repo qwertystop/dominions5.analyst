d989b2410fdc9d043b693a71ea06c8f9448a9a49
David Heyman <dheyman@fastmail.com>
2017-12-20 17:53:32 -0500

    Bytes reader

---
+++ apps/parser/lib/readers/bytes.ex
@@ -0,0 +1,10 @@
+defmodule Readers.Bytes do
+  @moduledoc """
+  For sequences of bytes, type unknown
+  """
+  @spec read!(Enumerable.t, pos_integer) :: {binary, :bytes, pos_integer}
+  def read!(input_bytestream, count) do
+    value = for <<b::binary-1 <- Enum.take(input_bytestream, count)>>, do: b
+    {value, :bytes, count}
+  end
+end
