7ccd401b14e0277bd36f360f624e261de45fdc07
David Heyman <dheyman@fastmail.com>
2017-12-21 11:55:53 -0500

    DelayedEvents moved to nonmacro

--- apps/parser/lib/subsections/delayed_events.ex
+++ apps/parser/lib/subsections/delayed_events.ex
@@ -1,13 +1,20 @@
 defmodule Parser.Subsections.DelayedEvents do
+  @behaviour Parser
   @moduledoc "Don't actually care about this but need to be sure of the size"
-  defp read_triplearray(input_bytestream) do
+  alias Parser.Readers.DomInteger
+  def read!(input_bytestream) do
+    get_next = fn -> DomInteger.read!(input_bytestream, 4) end
+    # Three arrays of int32 that share one leading index
     <<count::integer-little-32>> = Enum.take(input_bytestream, 4)
-    contents = Enum.take(input_bytestream, count * 3)
-    result = [count, (for <<val::integer-little-32 <- contents>>, do: val)]
-    {result, :special, count * 3 + 1}
+    [base, turn, lunar] =
+      for(_ <- 1..count, do: {get_next.(), get_next.(), get_next.()})
+        |> Enum.reduce([[],[],[]],
+                       fn ({b, t, l}, [base, turn, lunar]) ->
+                         [[b|base], [t|turn], [l|lunar]]
+                       end)
+    base = {Enum.reverse(base), {:list, :integer}, 4 * count}
+    turn = {Enum.reverse(turn), {:list, :integer}, 4 * count}
+    lunar = {Enum.reverse(lunar), {:list, :integer}, 4 * count}
+    {[{"base", base}, {"turn", turn}, {"lunar", lunar}], :section, (count * 3 * 4) + 4}
   end
-
-  #use Parser.Unpack, [
-  #  special(:event_array, read_triplearray/1)
-  #]
 end
