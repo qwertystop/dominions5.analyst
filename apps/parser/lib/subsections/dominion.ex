1d6f195b597aca83acedf8943b8dae94263be732
David Heyman <dheyman@fastmail.com>
2017-12-21 12:18:04 -0500

    Dominion moved to nonmacro

--- apps/parser/lib/subsections/dominion.ex
+++ apps/parser/lib/subsections/dominion.ex
@@ -1,7 +1,18 @@
 defmodule Parser.Subsections.Dominion do
-  #use Parser.Unpack, [
-  #  unknown(:bytes6, 6), string(:name),
-  #  unknown(:u32_00), unknown(:u32_01),
-  #  map(:unknownMap, {:integer, 4}, {:integer, 4})
-  #]
+  @behaviour Parser
+  alias Parser.Readers.{Bytes,DomInteger,DomString,DomMap}
+  @constant_part_size 6+4+4
+  def read!(input) do
+    unknownBytes = Bytes.read!(input, 6)
+    name = {_, :string, nameSize} = DomString.read!(input)
+    u32_00 = DomInteger.read!(input, 4, :unsigned)
+    u32_01 = DomInteger.read!(input, 4, :unsigned)
+    unknownMap = {_, {:map, :integer}, mapSize} = DomMap.read!(input, {:integer, 4}, {:integer, 4})
+    {[{"unknown bytes", unknownBytes},
+      {"name", name},
+      {"u32_00", u32_00},
+      {"u32_01", u32_01},
+      {"unknown map", unknownMap}],
+     :section, @constant_part_size + nameSize + mapSize}
+  end
 end
