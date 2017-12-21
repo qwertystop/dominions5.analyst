76f53efeb3fc58aa35e11d2ab9ee2909c53964f4
David Heyman <dheyman@fastmail.com>
2017-12-21 17:10:58 -0500

    Arb. masks for DomString

--- apps/parser/lib/readers/dom_string.ex
+++ apps/parser/lib/readers/dom_string.ex
@@ -4,23 +4,27 @@ defmodule Readers.DomString do
   """
   import Bitwise, only: [bxor: 2]
 
-  @spec read!(Enumerable.t) :: Parser.field
-  def read!(input_bytestream) do
+  @spec read!(Enumerable.t, keyword) :: Parser.field
+  def read!(input_bytestream, mask: mask) do
     <<value::binary>> = Enum.reduce_while(
       input_bytestream, <<>>,
       fn <<ch::8>>, acc ->
-        val = <<acc::binary, bxor(ch, 0x4f)>>
-        if ch == 0x4f, do: {:halt, val}, else: {:cont, val}
+        val = <<acc::binary, bxor(ch, mask)>>
+        if ch == mask, do: {:halt, val}, else: {:cont, val}
       end)
       {value, :string, byte_size(value)}
   end
 
-  @spec read!(Enumerable.t, pos_integer) :: Parser.field
-  def read!(input_bytestream, count) do
+  def read!(input_bytestream, length: count, mask: mask)
+  when count >= 1 do
     <<value::binary>> = for <<c::8>> <-
       Enum.take(input_bytestream, count),
       into: <<>>,
-      do: bxor(c, 0x4f)
+      do: bxor(c, mask)
     {value, :string, count}
   end
+
+  def read!(input_bytestream, []), do: read!(input_bytestream, mask: 0x4f)
+  def read!(input_bytestream, length: count),
+    do: read!(input_bytestream, length: count, mask: 0x4f)
 end
